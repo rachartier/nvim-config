741d3b325408b4fa14cbf17acddfd425fa06bf64
Raphaël CHARTIER <raphael.chartier@michelin.com>
2023-01-30 16:47:10 +0100

    enable dap and add event for dapui

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,18 @@
 require('dap.ext.vscode').load_launchjs()
 require("nvim-dap-virtual-text").setup()
-vim.fn.sign_define('DapBreakpoint', {text='🔴', texthl='', linehl='', numhl=''})
-vim.fn.sign_define('DapStopped', {text='❯'} )
+
+vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+
+require("dapui").setup()
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
